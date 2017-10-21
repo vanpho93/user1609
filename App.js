2738d4915670bff9fb2860bcabac26a25594cf8a
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-21 10:30:51 +0700

    sign in com

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { SignIn } from './components/SignIn';
-import { GoPlay } from './components/GoPlay';
+import { SignIn } from './SignIn';
+import { GoPlay } from './GoPlay';
 import { StackNavigator } from 'react-navigation';
 
 const Stack = StackNavigator({
   SignIn: { screen: SignIn },
   GoPlay: { screen: GoPlay }
-});
+}, { headerMode: 'none' });
 
 export default class App extends Component {
   render() {
--- SignIn.js
+++ SignIn.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
 
 export class SignIn extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { email: '', password: '' };
+        this.onSignIn = this.onSignIn.bind(this);
+    }
+
+    onSignIn() {
+        const { email, password } = this.state;
+        console.log(email, password);
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <TouchableOpacity onPress={() => this.props.navigation.navigate('GoPlay')}>
-                    <Text>Switch to Go Play</Text>
+                <TextInput 
+                    placeholder="Email"
+                    style={styles.input}
+                    onChangeText={email => this.setState({ email })}
+                />
+                <TextInput 
+                    placeholder="Password"
+                    style={styles.input}
+                    onChangeText={password => this.setState({ password })}
+                />
+                <TouchableOpacity onPress={this.onSignIn} style={styles.button}>
+                    <Text style={styles.buttonText}>Sign In</Text>
                 </TouchableOpacity>
             </View>
         );
@@ -19,5 +40,27 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#CAE3FC'
+    },
+    input: {
+        backgroundColor: '#FFFFFF',
+        width: 300,
+        padding: 5,
+        margin: 5,
+        borderRadius: 5,
+        height: 40
+    },
+    button: {
+        width: 300,
+        padding: 5,
+        margin: 5,
+        borderRadius: 5,
+        backgroundColor: '#FBFB00',
+        height: 40,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttonText: {
+        fontSize: 20,
+        color: 'purple'
     }
 });
